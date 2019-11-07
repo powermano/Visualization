09f1bec0585d17a3860a1388e7f2adeab664889d
tao.cai <tao.cai@gpu-dev005.hogpu.cc>
2019-11-07 14:40:48 +0800

    1

--- script/run_predict_parallel.py
+++ script/run_predict_parallel.py
@@ -22,7 +22,7 @@ data type info:
 if __name__ == '__main__':
     is_parallel_run = True
     save_feature = '1'
-    save_badcase = '1'
+    save_badcase = '0'
 
     test_type = 'X1600_v2'
     test_file = '/home/users/tao.cai/aiot_face_anti_spoofing_tools/predict/test_list/{}_test.txt'.format(
@@ -34,9 +34,9 @@ if __name__ == '__main__':
     input_format = 'rgb'
     is_qnn = '0'
 
-    model_dir = '/home/users/tao.cai/Workspace/anti_spoof/gluonface/0815/triplet_loss/margin-0.2/awl-twice/'
+    model_dir = '/home/users/tao.cai/Workspace/anti_spoof/gluonface/wobn/triplet_loss/margin-0.2/awl-l2-wo-sum-ratio-1.5-twice/'
     model_list = ['gpu']
-    epoch_list = [16]
+    epoch_list = [55]
     data_type_list = [4]
     expand_ratio_list = [1.5]
 
@@ -47,10 +47,10 @@ if __name__ == '__main__':
                 test_type, model, epoch, data_type, expand_ratio)
         else:
             prefix = model_dir + 'model-{}'.format(str(model))
-            work_dir = './results/{}/python3.4.5_0815_triplet_loss_fix_margin-0.2_awl_twice_feature_gpu_model{}_epoch{}_dataType{}_expandRatio{}'.format(
-                        test_type, model, epoch, data_type, expand_ratio)
-          #  work_dir = './results/{}/python3.4.5_0815_wobn_triplet_loss_margin-0.2_l2_wo_sum_feature_gpu_model{}_epoch{}_dataType{}_expandRatio{}'.format(
-          #               test_type, model, epoch, data_type, expand_ratio)
+          #  work_dir = './results/{}/python3.4.5_0815_triplet_loss_fix_margin-0.2_awl_l2_wo_sum_twice_feature_gpu_model{}_epoch{}_dataType{}_expandRatio{}'.format(
+         #               test_type, model, epoch, data_type, expand_ratio)
+            work_dir = './results/{}/python3.4.5_0815_wobn_triplet_loss_margin-0.2_l2_wo_sum_ratio_1.5_twice_feature_gpu_model{}_epoch{}_dataType{}_expandRatio{}'.format(
+                         test_type, model, epoch, data_type, expand_ratio)
         if not os.path.exists(work_dir):
             os.makedirs(work_dir)
         os.chdir(work_dir)
